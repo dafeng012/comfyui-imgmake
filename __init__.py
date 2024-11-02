8ea0b147c6fa0c304351de27b4a5188125c16673
dafeng012 <563812237@qq.com>
2024-11-02 14:21:42 +0800

    改错

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-from img_ebsynth.image2mask import *
-from img_ebsynth.video2image import *
-from img_ebsynth.gj_image import *
-from img_ebsynth.load_images_nodes import *
-from img_ebsynth.image_name_slect import *
-from img_ebsynth.ebs import *
-from img_ebsynth.main import *
+from .img_ebsynth.image2mask import image2mask,SaveImageExtended
+from .img_ebsynth.video2image import Video2Frames,LoadImageListPlus
+from .img_ebsynth.gj_image import VideoKeyFramesExtractor
+from .img_ebsynth.load_images_nodes import LoadImagesFromPath_lp
+from .img_ebsynth.image_name_slect import SelectImageName
+from .img_ebsynth.ebs import ebsynth_process,ebsynth_hecheng
+from .img_ebsynth.main import ebsynth_main
 NODE_CLASS_MAPPINGS = {
     "image2mask": image2mask,
     "video2image": Video2Frames,
